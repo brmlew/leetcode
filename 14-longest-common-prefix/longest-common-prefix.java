40f99057421ad636cdee6202b4d3ece339ad2e5d
brmlew <63668155+brmlew@users.noreply.github.com>
2024-04-23 13:06:47 -0400

    Time: 4 ms (20.57%) | Memory: 44.5 MB (5.11%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -6,9 +6,6 @@ class Solution {
         for (int i = 0; i <= strs[0].length(); i++) {
             String sub = strs[0].substring(0, i);
 
-            String test = "a";
-            System.out.println(test.substring(0,1));
-
             for (int j = 1; j < strs.length; j++) {
                 if (i > strs[j].length()) return result;
                 String temp = strs[j].substring(0,i);
