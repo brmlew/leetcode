99a4fb0bb57d5b75dd524ba1aa0040cdc774a0a0
brmlew <63668155+brmlew@users.noreply.github.com>
2024-04-30 19:51:52 -0400

    Time: 2115 ms (44.11%) | Memory: 12.6 MB (21.27%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if (i != j and nums[i] + nums[j] == target):
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
                     return [i, j]
         
