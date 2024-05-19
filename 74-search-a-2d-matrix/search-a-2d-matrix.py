827f1644dd5531c1b73c978ae0f63b8c471440d1
brmlew <63668155+brmlew@users.noreply.github.com>
2024-05-19 13:31:51 -0400

    Time: 33 ms (5.14%) | Memory: 12 MB (50.40%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        low, high = 0, len(matrix)
+        while low <= high:
+            if low + 1 == high: break
+            mid = (low + high) // 2
+            if target == matrix[mid][0]: return True
+            elif target > matrix[mid][0]:
+                low = mid
+            else: 
+                high = mid
+        row = low
+        print(row)
+        low, high = 0, len(matrix[row])
+        while low <= high:
+            if matrix[row][low] == target: return True
+            if low + 1 == high: break
+            mid = (low + high) // 2
+            if target == matrix[row][mid]: return True
+            elif target > matrix[row][mid]:
+                low = mid
+            else: 
+                high = mid
+        
+        return False
+            
