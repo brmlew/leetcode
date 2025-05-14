a3a1d732741b1452efe6511d0de4487dc8db044e
brmlew <63668155+brmlew@users.noreply.github.com>
2025-05-14 15:02:14 -0400

    Time: 21 ms (16.26%) | Memory: 42.4 MB (84.79%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)) && t.charAt(i) != map.get(s.charAt(i))) {
+                return false;
+            } else if (!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) {
+                return false;
+            }
+            map.put(s.charAt(i), t.charAt(i));
+        }
+        return true;
+    }
+}
