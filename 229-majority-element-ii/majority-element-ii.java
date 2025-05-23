83df42df03c0162290f08ef7975d48d8ba608728
brmlew <63668155+brmlew@users.noreply.github.com>
2025-05-23 16:22:52 -0400

    Time: 18 ms (7.96%) | Memory: 49.9 MB (26.57%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -4,14 +4,15 @@ class Solution {
         List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1); 
-                
-            } else {
-                map.put(nums[i], 1);
-            }
-            if (map.get(nums[i]) > nums.length / 3 && !result.contains(nums[i])) {
-                result.add(nums[i]);
+            if (!result.contains(nums[i])) {
+                if (map.containsKey(nums[i])) {
+                    map.put(nums[i], map.get(nums[i]) + 1); 
+                } else {
+                    map.put(nums[i], 1);
+                }
+                if (map.get(nums[i]) > nums.length / 3) {
+                    result.add(nums[i]);
+                }
             }
         }
 
